e69615abd7f91cca7a75c807bd47a9b297325be0
ManuelM512 <manuel5mongelos@gmail.com>
2022-10-31 10:10:02 -0300

    Added characters to character factory

---
+++ test/LibraryTests/EscenarioTest.cs
@@ -0,0 +1,18 @@
+using RoleplayGame.Scenarios;
+using RoleplayGame.Items;
+using RoleplayGame.Characters;
+using RoleplayGame.Encounters;
+namespace LibraryTests;
+
+public class EscenarioTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestEscenario()
+    {
+    }
+}
